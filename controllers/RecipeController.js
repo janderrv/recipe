7743e11ff9556ec5fb612751fe2e366605d4e043
Jander <janderrv26@gmail.com>
2020-04-29 21:09:45 -0400

    Exibindo receitas na tela inicial

--- controllers/RecipeController.js
+++ controllers/RecipeController.js
@@ -12,7 +12,26 @@ class RecipeController {
             console.log(error);
         }
     }
-
+    getRecipeById = async (id) => {
+        try {
+            const response = await axios.get(url + 'lookup.php?i=' + id);
+            return response.data.meals[0];
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    getRandomRecipe = async () => {
+        let recipes = []
+        try {
+            for (let i = 0; i < 9; i++) {
+                const response = await axios.get(url + 'random.php');
+                recipes.push(response.data.meals[0]);
+            }
+            return recipes;
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
 module.exports = new RecipeController();
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const RecipeController = require('../controllers/RecipeController');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Express'
-  });
+  getRecipe = async () => {
+    let recipes = await RecipeController.getRandomRecipe();
+    res.render('index', {
+      title: 'Recipes',
+      recipes
+    })
+  }
+  getRecipe();
 });
 
 router.get('/letter/:id', function (req, res, next) {
   let id = req.params.id;
 
   getByLetter = async () => {
-    let response = await RecipeController.getRecipeByLetter(id);
-    res.json(response.meals);
+    try {
+      let response = await RecipeController.getRecipeByLetter(id);
+      res.json(response.meals);
+    } catch (error) {}
   }
   getByLetter();
 });
+router.get('/id/:id', function (req, res, next) {
+  let id = req.params.id;
+
+  getByLetter = async () => {
+    try {
+      let response = await RecipeController.getRecipeById(id);
+      res.json(response);
+    } catch (error) {}
+  }
+  getByLetter();
+});
+
 
 module.exports = router;
